9f9d85def61faa23a60fc34a2b256ac3e61eef68
topcatsss <coding.tris@the-owls.org>
2026-02-07 20:38:37 +0000

    Python 3 Updated Login and exception catching

--- README.md
+++ README.md
@@ -1,15 +1,15 @@
 # raspberry-zwave-python
+
 Raspberry PI Z-Wave board Python Interface
 Provides a quick and easy interface into the ZWAY Automation API
 
->This was orignally  written for Python 2.7
-
->But there now is a Python 3 version.
+> This was orignally  written for Python 2.7.  
+> But there now is a Python 3 version.
 
 *Due to the way Python 2 and Python 3 work, there are 2 different libraries, but the calls and functionally remains the same.*
 
-
 ## How to Use - Python 3
+
 ```python
 from zWaveApi3 import *
 
@@ -18,6 +18,7 @@ oZDevices = oZWave.getDevices()
 ```
 
 ## How to Use - Python 2
+
 ```python
 from zWaveApi import *
 
@@ -25,33 +26,33 @@ oZWave = zWaveApi('username', 'password', 'zwaveurl')
 oZDevices = oZWave.getDevices()
 ```
 
-
 ## Requires
+
 * json
 * Python 3
-    * http.client
-    * urllib.parse
+  * http.client
+  * json
 
 * Python 2:
-    * urllib2
-    * urllib
-    * httplib
-
+  * urllib2
+  * urllib
+  * httplib
 
 ## Functions
 
 ### getDevices()
+
 Returns a list of zWave Devices as a JSON array
 
 ### setDeviceCommand(deviceid, newcommand)
+
 Sends a Command to a zWave Device
 
-> **deviceid** - The unique zWave device ID
+* **deviceid** - The unique zWave device ID
+* **newcommand** - The Require command for the device  
+    This depends on the device type, see below
+  * switchMultilevel : on / off / min / max / exact?level=40 / increase / decrease / update
+  * switchBinary     : on / off / update
+  * toggleButton     : on
 
-> **newcommand** - The Require command for the device
-                         This depends on the device type, see below
->* switchMultilevel : on / off / min / max / exact?level=40 / increase / decrease / update
->* switchBinary     : on / off / update
->* toggleButton     : on
-
-> **Returns** either 1 or 0 dependant on command being accepted
+**Returns** either 1 or 0 dependant on command being accepted
--- zWaveApi3.py
+++ zWaveApi3.py
@@ -1,11 +1,10 @@
 import http.client
-import urllib.parse
 import json
 
 class zWaveApi3(object):
-    '''
+    """
     zWave zWay Automation API access
-        
+
     This is a library for Python 3
 
     from https://github.com/topcats/raspberry-zwave-python
@@ -13,20 +12,31 @@ class zWaveApi3(object):
     Methods:
         getDevices       - Gets a full List of zWave Devices
         setDeviceCommand - Sends a command to a zWave Device
-    
+
     Init Variables:
         zUsername - Username with access to API
         zPassword - password for API
         zBaseUrl  - full url for the ZAutomation api path
-    '''
+    """
     zlogin_cookie = ''
     zlogin_username = ''
     zlogin_password = ''
     zlogin_apibaseURL = 'http://localhost:8083/ZAutomation/api/v1/'
-    zlogin_server = ''
-    zlogin_serverpath = ''
+    strServer = 'localhost:8083'
+    strServerPath = ''
+
 
     def __init__(self, zUsername, zPassword, zBaseUrl):
+        """
+        zWave zWay Automation API access
+
+        :param zUsername: Username with access to API
+        :type zUsername: string
+        :param zPassword: password for API
+        :type zPassword: string
+        :param zBaseUrl: full url for the ZAutomation api path
+        :type zBaseUrl: string
+        """
         self.zlogin_username = zUsername
         self.zlogin_password = zPassword
         self.zlogin_apibaseURL = zBaseUrl
@@ -34,78 +44,108 @@ class zWaveApi3(object):
         self.strServer = self.strServer[:self.strServer.index('/')]
         self.strServerPath = self.zlogin_apibaseURL.replace('http://'+self.strServer,'')
 
-    def DoLogin(self):
-        '''Does the initial authentication before anything else
-        Is run only when needed, you do not need to run directly'''
-        post_login = urllib.parse.urlencode({'form': 'true', 'login': self.zlogin_username, 'password': self.zlogin_password, 'keepme': 'false', 'default_ui': 1})
 
-        webheaders = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"}
-        webconn = http.client.HTTPConnection(self.strServer)
-        webconn.request("POST", self.strServerPath+'login', post_login, webheaders)
-        webresponse = webconn.getresponse()
-        webresponseCookie = webresponse.getheader('set-cookie')#['ZWAYSession']
-        webresponseCookie = webresponseCookie.replace(' ','')
-        webresponseCookie = webresponseCookie[webresponseCookie.index('ZWAYSession='):].replace('ZWAYSession=','')
-        webresponseCookie = webresponseCookie[:webresponseCookie.index(';')]
-        self.zlogin_cookie = webresponseCookie
-        webconn.close()
-        return webresponse.status
+    def __DoLogin(self):
+        """
+        Does the initial authentication before anything else
+        Is run only when needed, you do not need to run directly.
+
+        :return: HTTP Response code from login attempt
+        :rtype: integer
+        """
+
+        try:
+            post_login = json.dumps({'form': True, 'login': self.zlogin_username, 'password': self.zlogin_password, 'keepme': False, 'default_ui': 1})
+            webheaders = {"Content-type": "application/json", "Accept": "application/json"}
+            webconn = http.client.HTTPConnection(self.strServer)
+            webconn.request("POST", self.strServerPath+'login', body=post_login.encode(), headers=webheaders, encode_chunked=False)
+            webresponse = webconn.getresponse()
+            webresponseCookie = webresponse.getheader('set-cookie') # ['ZWAYSession']
+            webresponseCookie = webresponseCookie.replace(' ','')
+            webresponseCookie = webresponseCookie[webresponseCookie.index('ZWAYSession='):].replace('ZWAYSession=','')
+            webresponseCookie = webresponseCookie[:webresponseCookie.index(';')]
+            self.zlogin_cookie = webresponseCookie
+            webconn.close()
+            return webresponse.status
+        except:
+            self.zlogin_cookie == ''
+            return 0
 
 
     def getDevices(self):
-        '''Get a list of all zWave Devices
-        Return a JSON object of Devices'''
+        """
+        Get a list of all zWave Devices
 
-        #Double check login
+        :return: JSON Object of zWave Devices
+        :rtype: JSON Object
+        """
+
+        # Double check login
         if (self.zlogin_cookie == ''):
-            self.DoLogin()
+            self.__DoLogin()
 
-        webheaders = {'Content-type': 'application/json', "Accept": "application/json", "Cookie": "ZWAYSession="+self.zlogin_cookie}
-        webconn = http.client.HTTPConnection(self.strServer)
-        webconn.request("GET", self.strServerPath+'devices?since=0', '', webheaders)
-        webresponse = webconn.getresponse()
-        webdata = webresponse.read()
-        webconn.close()
-        json_obj = json.loads(webdata.decode())
-        return json_obj['data']['devices']
+        try:
+            if (self.zlogin_cookie == ''):
+                raise ValueError('Login Failed')
+            webheaders = {'Content-type': 'application/json', "Accept": "application/json", "Cookie": "ZWAYSession="+self.zlogin_cookie}
+            webconn = http.client.HTTPConnection(self.strServer)
+            webconn.request("GET", self.strServerPath+'devices?since=0', '', webheaders)
+            webresponse = webconn.getresponse()
+            webdata = webresponse.read()
+            webconn.close()
+            json_obj = json.loads(webdata.decode())
+            return json_obj['data']['devices']
+        except:
+            return []
 
 
     def setDeviceCommand(self, deviceid, newcommand):
-        '''Send a Command to a zWave Device
+        """
+        Send a Command to a zWave Device
 
         Init Variables:
             deviceid   - The unique zWave device ID
             newcommand - The Require command for the device
                          This depends on the device type, see below
-        
+
         switchMultilevel : on / off / min / max / exact?level=40 / increase / decrease / update
         switchBinary     : on / off / update
         toggleButton     : on
-        
-        Returns either 0 or 1 if successful'''
 
-        #Double check login
+        :param deviceid: zWave Device ID
+        :type deviceid: string
+        :param newcommand: Command to send to Device
+        :type newcommand: string
+        :return: 1 if successful, 0 if failed
+        :rtype: integer
+        """
+
+        # Double check login
         if (self.zlogin_cookie == ''):
-            self.DoLogin()
+            self.__DoLogin()
 
-        #Check inputs
+        # Check inputs
         if (deviceid == ''):
             return 0
         if (newcommand == ''):
             return 0
 
-        #Do Command
-        webheaders = {'Content-type': 'application/json', "Accept": "application/json", "Cookie": "ZWAYSession="+self.zlogin_cookie}
-        webconn = http.client.HTTPConnection(self.strServer)
-        webconn.request("GET", self.strServerPath+'devices/'+deviceid+'/command/'+newcommand, '', webheaders)
-        webresponse = webconn.getresponse()
-        webdata = webresponse.read()
-        webconn.close()
+        # Do Command
+        try:
+            if (self.zlogin_cookie == ''):
+                raise ValueError('Login Failed')
+            webheaders = {'Content-type': 'application/json', "Accept": "application/json", "Cookie": "ZWAYSession="+self.zlogin_cookie}
+            webconn = http.client.HTTPConnection(self.strServer)
+            webconn.request("GET", self.strServerPath+'devices/'+deviceid+'/command/'+newcommand, '', webheaders)
+            webresponse = webconn.getresponse()
+            webdata = webresponse.read()
+            webconn.close()
 
-        #Check Response
-        json_obj = json.loads(webdata.decode())
-        if (json_obj['code'] == 200):
-            return 1
-        else:
+            # Check Response
+            json_obj = json.loads(webdata.decode())
+            if (json_obj['code'] == 200):
+                return 1
+            else:
+                return 0
+        except:
             return 0
-
